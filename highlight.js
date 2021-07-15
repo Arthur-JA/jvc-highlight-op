9483c2f013ad8c01923a3a486daff41114b2b6ac
Arthur-JA <Arthur-JA@users.noreply.github.com>
2021-07-15 02:58:00 +0200

    Update highlight.js

--- highlight.js
+++ highlight.js
@@ -11,36 +11,33 @@ const parser = new DOMParser();
 const msgContainer = document.querySelector('.conteneur-messages-pagi');
 const pager = msgContainer.querySelector('.bloc-pagi-default .bloc-liste-num-page');
 
-if (!msgContainer || !pager) {
-    return;
-}
+if (pager) {
+    let opName = null;
 
-let opName = null;
+    if (pager.querySelector('.page-active').textContent == '1') {
+        opName = msgContainer.querySelector('.bloc-pseudo-msg').textContent;
+        highlightOp();
+    }
+    else {
+        const httpRequest = new XMLHttpRequest();
+        httpRequest.onreadystatechange = () => {
+            if (httpRequest.readyState === XMLHttpRequest.DONE && httpRequest.status === 200) {
+                const doc = parser.parseFromString(httpRequest.responseText, "text/html");
+                opName = doc.querySelector('.bloc-pseudo-msg').textContent;
+                highlightOp();
+            }
+        }
+        httpRequest.open('GET', pager.querySelector('a.lien-jv').href, true);
+        httpRequest.send();
+    }
 
-if (pager.querySelector('.page-active').textContent == '1') {
-    opName = msgContainer.querySelector('.bloc-pseudo-msg').textContent;
-    highlightOp();
-}
-else {
-    const httpRequest = new XMLHttpRequest();
-    httpRequest.onreadystatechange = () => {
-        if (httpRequest.readyState === XMLHttpRequest.DONE && httpRequest.status === 200) {
-            const doc = parser.parseFromString(httpRequest.responseText, "text/html");
-            opName = doc.querySelector('.bloc-pseudo-msg').textContent;
-            highlightOp();
+    function highlightOp() {
+        if (opName) {
+            msgContainer.querySelectorAll('.bloc-pseudo-msg').forEach(elem => {
+                if (elem.textContent === opName) {
+                    elem.innerHTML += ('<strong style="color: #FF6600;font-size: 12px;margin-left: 5px;vertical-align:bottom">OP</strong>');
+                }
+            });
         }
     }
-    httpRequest.open('GET', pager.querySelector('a.lien-jv').href, true);
-    httpRequest.send();
 }
-
-function highlightOp() {
-    if (opName) {
-        msgContainer.querySelectorAll('.bloc-pseudo-msg').forEach(elem => {
-            if (elem.textContent === opName) {
-                elem.innerHTML += ('<strong style="color: #FF6600;font-size: 12px;margin-left: 5px;vertical-align:bottom">OP</strong>');
-            }
-        });
-    }
-}
-
